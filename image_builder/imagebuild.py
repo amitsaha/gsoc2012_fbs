21159a734be8fd8b96cde5d8ade59f318e5de635
Amit Saha <amitsaha.in@gmail.com>
2012-05-27 02:59:36 +1000

    imagebuild.py: Added support for creating live images; removed command line options; uses config files

--- image_builder/imagebuild.py
+++ image_builder/imagebuild.py
@@ -22,41 +22,35 @@ import sys
 import os
 import argparse
 import ConfigParser
-import koji
 import subprocess
+import koji
+import yum
 from image_builder.repo_create import RepoCreate
 from image_builder.bootiso import Bootiso
 from pykickstart.parser import *
 from pykickstart.version import makeVersion
 
-
-possible_types = ['boot','live','dvd']
-possible_arches = ['x86_64', 'i386','i686']
+# globals
+possible_types = ['boot','dvd','live']
+possible_arches = ['i386','i686','x86_64']
 possible_products = ['fedora']
-possible_releases = ['16', '17', 'rawhide']
+possible_releases = ['17', 'rawhide']
 
-#configuration files
-repo_config = 'repoinfo.conf'
-pungi_config = 'pungi.conf'
+
+#Install image specifications
+build_config='config/imagebuild.conf'
+
+boot_config = 'config/boot.conf'
+#repository config
+repo_config = 'config/repoinfo.conf'
+pungi_config= 'config/pungi.conf'
+live_config = 'config/live.conf'
 
 # Koji
 kojihub_url = 'http://koji.fedoraproject.org/kojihub'
 
 # Koji connection object
 conn=koji.ClientSession(kojihub_url)
-
-def main_func():
-    pass
-
-# Boot ISO
-def build_bootiso(arch, release, version, repos, mirrors, proxy, outputdir,product):
-    boot_builder = Bootiso(arch, release, version, repos, mirrors, proxy, outputdir,product)
-    boot_builder.make_iso()
-
-# Live Image
-def build_live(arch, release, version, repos, proxy, outputdir,product):
-    live_builder = Live(arch, release, version, repos, proxy, outputdir,product)
-    live_builder.make_live()
 
 
 # Add a repository to an existing KS file
@@ -77,11 +71,136 @@ def add_repo(ksfile,siderepo):
     outfile.close()
 
 
+
+# get NVR given build ID
+def get_nvr(bids,arch):
+
+    nvr=[]
+    for bid in bids:
+        # get the build information for this bid
+        build=conn.getBuild(int(bid))
+        # NVR
+        package=build['nvr']
+        
+        nvr.append(package)
+    
+    return nvr
+
+# prepare a side repository given extra packages
+def prep_siderepo(workdir, packages, arch):
+    arch = [arch]
+    arch.append('noarch')
+
+
+    repodir = '%s/siderepo' % workdir
+    repo_create = RepoCreate(repodir,arch)
+    repo_create.make_repo(packages)
+
+    repo_url = 'file://%s' % repodir
+    return repo_url
+
+
+# Gather repository list from repoinfo.conf
+def gather_repos(release, arch):
+    # read repo configuration
+    
+    # The mirrorlist and repositories uses i386 as $arch, instead of
+    # i686
+    if arch=='i686':
+        arch='i386'
+
+    config = ConfigParser.SafeConfigParser({'arch':arch})
+    config.read(repo_config)
+
+    reponames = [ release ]
+
+    updates=config.get('DEFAULT','updates')
+    testing=config.get('DEFAULT','updates-testing')
+
+    if updates=='1':
+        reponames.append('%s-updates' % release)
+
+    if testing=='1':
+        reponames.append('%s-updates-testing' % release)
+
+    repos = []
+    mirrors = []
+    for name in reponames:
+        # repos
+        repos.append(config.get(name, 'url'))
+        # mirrorlist
+        mirrors.append(config.get(name,'mirror'))
+
+    return repos,mirrors
+    
+
+# Boot ISO
+def build_bootiso():
+
+    # Read pungi configuration
+    config = ConfigParser.SafeConfigParser()
+    config.read(boot_config)
+
+    arch=config.get('DEFAULT','arch')
+    outdir=config.get('DEFAULT','outdir')
+    workdir=config.get('DEFAULT','workdir')
+    product=config.get('DEFAULT','product')
+    release=config.get('DEFAULT','release')
+    version=config.get('DEFAULT','version')
+    proxy=config.get('DEFAULT','proxy')
+    
+    if proxy=='':
+        proxy=None
+
+    if not arch in possible_arches:
+        print "ISOs for arch %s are not supported" % arch
+        sys.exit(1)
+
+    if not product in possible_products:
+        print "Product %s is not supported" % product
+
+    if not release in possible_releases:
+        print "Release %s is not supported" % release
+        sys.exit(1)
+
+
+    # gather repos and mirrors
+    repos,mirrors = gather_repos(release,arch)
+
+    # Create the side repository if needed
+    nvr=config.get('DEFAULT','nvr')
+    bid=config.get('DEFAULT','bid')
+
+    rpms_nvr=[]
+    if nvr!='':
+        for rpm in nvr.split(';'):
+            rpms_nvr.append(rpm)
+
+    bids=[]
+    if bid!='':
+        for rpm in bid.split(';'):
+            bids.extend(str(rpm))
+    
+    if len(bids)>0:
+        rpms_bid = get_nvr(bids,arch)
+        rpms_nvr.extend(rpms_bid)
+    
+    # prepare side repository
+    rpms = rpms_nvr
+
+    if len(rpms) > 0:
+        siderepo = prep_siderepo(workdir, rpms, arch)
+        repos.append(siderepo)
+
+    boot_builder = Bootiso(arch, release, version, repos, mirrors, proxy, outdir,product)
+    boot_builder.make_iso()
+
+
 # DVD Image
 def build_dvd():
 # See: https://fedorahosted.org/pungi/wiki/PungiDocs/RunningPungi
 # TODO: Architecture - if the desired architecture sought is different from the
-# host arch, use 'mock'
+# host arch, use 'mock (?)'
 
     # Read pungi configuration
     config = ConfigParser.SafeConfigParser()
@@ -138,6 +257,9 @@ def build_dvd():
     nvr=config.get('DEFAULT','nvr')
     bid=config.get('DEFAULT','bid')
 
+    arch=config.get('DEFAULT','arch')
+    workdir=config.get('DEFAULT','workdir')
+
     rpms_nvr=[]
     if nvr!='':
         for rpm in nvr.split(';'):
@@ -153,8 +275,6 @@ def build_dvd():
         rpms_nvr.extend(rpms_bid)
     
     # prepare side repository
-    arch=config.get('DEFAULT','arch')
-    workdir=config.get('DEFAULT','workdir')
     rpms = rpms_nvr
     if len(rpms) > 0:
         siderepo = prep_siderepo(workdir, rpms, arch)
@@ -167,155 +287,110 @@ def build_dvd():
 
     subprocess.call(process_call)
 
-def get_nvr(bids,arch):
 
-    nvr=[]
-    for bid in bids:
-        # get the build information for this bid
-        build=conn.getBuild(int(bid))
-        # NVR
-        package=build['nvr']
-        
-        nvr.append(package)
+# Live Image
+def build_live():
+
+    # Currently using livecd-creator
+    # as the forward moving livemedia-creator is not yet
+    # quite usable
+
+    # Read pungi configuration
+    config = ConfigParser.SafeConfigParser()
+    config.read(live_config)
+
+    args=[]
+
+    ks=config.get('DEFAULT','config')
+    args.extend(['-c', ks])
+
+    label=config.get('DEFAULT','label')
+    args.extend(['-f', label])
+
+    title=config.get('DEFAULT','title')
+    args.extend(['--title', title])
     
-    return nvr
+    product=config.get('DEFAULT','product')
+    args.extend(['--product', product])
 
-def prep_siderepo(workdir, packages, arch):
-    arch = [arch]
-    arch.append('noarch')
+    releasever=config.get('DEFAULT','releasever')
+    args.extend(['--releasever', releasever])
 
-
-    repodir = '%s/siderepo' % workdir
-    repo_create = RepoCreate(repodir,arch)
-    repo_create.make_repo(packages)
-
-    repo_url = 'file://%s' % repodir
-    return repo_url
-
-
-def gather_repos(release, arch):
-    # read repo configuration
+    arch=config.get('DEFAULT','arch')
+    yb = yum.YumBase()
     
-    # The mirrorlist and repositories uses i386 as $arch, instead of
-    # i686
-    if arch=='i686':
-        arch='i386'
-
-    config = ConfigParser.SafeConfigParser({'arch':arch})
-    config.read(repo_config)
-
-    reponames = [ release ]
-
-    updates=config.get('DEFAULT','updates')
-    testing=config.get('DEFAULT','updates-testing')
-
-    if updates=='1':
-        reponames.append('%s-updates' % release)
-
-    if testing=='1':
-        reponames.append('%s-updates-testing' % release)
-
-    repos = []
-    mirrors = []
-    for name in reponames:
-        # repos
-        repos.append(config.get(name, 'url'))
-        # mirrorlist
-        mirrors.append(config.get(name,'mirror'))
-
-    return repos,mirrors
+    if arch!=yb.arch.canonarch:
+        print 'Live image arch should be the same as the build arch'
+        sys.exit(1)
     
+    tmpdir=config.get('DEFAULT','tmpdir')
+    args.extend(['-t', tmpdir])
+
+    cachedir=config.get('DEFAULT','cachedir')
+    args.extend(['--cache', cachedir])
+
+    logfile=config.get('DEFAULT','logfile')
+    args.extend(['--logfile', logfile])
+
+
+    # Create the side repository if needed
+    nvr=config.get('DEFAULT','nvr')
+    bid=config.get('DEFAULT','bid')
+
+    rpms_nvr=[]
+    if nvr!='':
+        for rpm in nvr.split(';'):
+            rpms_nvr.append(rpm)
+
+    bids=[]
+    if bid!='':
+        for rpm in bid.split(';'):
+            bids.extend(str(rpm))
+    
+    if len(bids)>0:
+        rpms_bid = get_nvr(bids,arch)
+        rpms_nvr.extend(rpms_bid)
+    
+    # prepare side repository
+    rpms = rpms_nvr
+    arch=config.get('DEFAULT','arch')
+    tmpdir=config.get('DEFAULT','tmpdir')
+
+    if len(rpms) > 0:
+        siderepo = prep_siderepo(tmpdir, rpms, arch)
+        # Add side repo to the existing KS file
+        add_repo(ks,siderepo)
+    
+    # fire pungi
+    process_call = ['livecd-creator']
+    process_call.extend(args)
+
+    subprocess.call(process_call)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Build Test ISOs for Fedora')
-    parser.add_argument('-t', metavar = 'type', type = str, nargs = 1,
-                        default = 'boot',  help = 'Type of ISO to build: boot live DVD')
-    parser.add_argument('-a', metavar = 'arch', type = str, nargs = 1,
-                        default = 'x86_64', help = 'Arch of image: x86_64 i386')
-    parser.add_argument('-o', metavar = 'output_dir', type = str, nargs = 1,
-                        default = './images', help = 'Output directory')
-    parser.add_argument('-w', metavar = 'work_dir', type = str, nargs = 1,
-                        default = './work', help = 'Work directory for image building')
-    parser.add_argument('-p', metavar = 'product', type = str, nargs = 1,
-                        default = 'Fedora', help = 'Product to build')
-    parser.add_argument('-r', metavar = 'release', type = str, nargs = 1,
-                        help = 'Release of product to build')
-    parser.add_argument('-v', metavar = 'version', type = str, nargs = 1,
-                        help = 'Version of ISO to build')
-    parser.add_argument('--nvr', metavar = 'nvr', type = str, nargs = '+', default=[],
-                        help = 'Specify packages via NVR to download from Koji') 
-    parser.add_argument('--bid', metavar = 'bid', type = str, nargs = '+', default=[],
-                        help = 'Specify packages via buildids to be downloaded from Koji') 
-    
-    # parse
-    args = parser.parse_args()
 
-    iso_type = args.t[0]
+    # Read configuration
+    config = ConfigParser.SafeConfigParser()
+    config.read(build_config)
 
+    iso_type=config.get('DEFAULT','type')
 
     if not iso_type in possible_types:
-        print "ISOs of type %s are not supported" % iso_type
-        
+        print 'ISOs of type {0:s} are not supported'.format(iso_type)
         sys.exit(1)
 
     # boot
     if iso_type=='boot':
-        workdir = os.path.abspath(args.w[0])
-        if not os.path.exists(workdir):
-            os.makedirs(workdir)
-
-        outputdir = os.path.abspath(args.o[0])
-        if not os.path.exists(outputdir):
-            os.makedirs(outputdir)
-
-            
-        arch = args.a[0]
-        if not arch in possible_arches:
-            print "ISOs for arch %s are not supported" % arch
-            sys.exit(1)
-
-    #product = args.p[0].lowercase()
-        product = args.p[0]
-        if not product in possible_products:
-            print "Product %s is not supported" % product
-
-        release = args.r[0]
-        if not release in possible_releases:
-            print "Release %s is not supported" % release
-            sys.exit(1)
-
-        version = args.v[0]
-
-        repos,mirrors = gather_repos(release,arch)
-    
-         #RPMs by NVR
-        rpms_nvr=[]
-        if len(args.nvr)>0:
-            rpms_nvr = args.nvr
-
-         #RPMs by Build IDs
-        if len(args.bid)>0:
-            # get the NVR from the bids so that we can use the same code to 
-            # download the packages
-            rpms_bid = get_nvr(args.bid,arch)
-            # all rpms' NVR
-            rpms_nvr.extend(rpms_bid)
-    
-        # prepare side repository
-        rpms = rpms_nvr
-        if len(rpms) > 0:
-            siderepo = prep_siderepo(workdir, rpms, arch)
-            repos.append(siderepo)
-
-        print('Building Boot ISO')
-        build_bootiso(arch, release, version, repos, mirrors, None, outputdir, product)
-
+        print 'Building Boot ISO'
+        build_bootiso()
 
     # DVD
     if iso_type == 'dvd':
-        print('Building DVD')
+        print'Building DVD'
         build_dvd()
-
-    # if iso_type == 'live':
-    #     print('Building Live Image')
-    #     #build_live(arch, release, version, repos, None, outputdir, product)
-
+        
+    #Live image
+    if iso_type == 'live':
+        print 'Building Live Image'
+        build_live()
