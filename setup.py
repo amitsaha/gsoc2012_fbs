1061a78c021feb546530148f70419404064692e2
Amit Saha <amitsaha.in@gmail.com>
2012-06-10 00:52:20 +1000

    Setup file for image_builder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='image_builder',
+      version='1.0',
+      description='On-Demand Fedora Build Service Modules',
+      author='Amit Saha',
+      author_email='amitksaha@fedoraproject.org',
+      url='https://github.com/amitsaha/gsoc2012_fbs/',
+      packages=['image_builder'],
+     )
