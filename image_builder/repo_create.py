10fbea4b95e5dd535e1f843371315bb75446da40
Amit Saha <amitsaha.in@gmail.com>
2012-06-21 20:31:31 +1000

    image_builder/repo_create.py: cleanups, no globals

--- image_builder/repo_create.py
+++ image_builder/repo_create.py
@@ -13,7 +13,8 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
+# 02110-1301, USA.
 
 # Contact: Amit Saha <amitksaha@fedoraproject.org>
 # 	 http://fedoraproject.org/wiki/User:Amitksaha
@@ -23,21 +24,25 @@ import koji
 import urllib
 import subprocess
 
-kojiurl = 'http://koji.fedoraproject.org/kojihub'
-pkgurl = 'http://koji.fedoraproject.org/packages'
-
 class RepoCreate(object):
+    """ Create side repository with additional packages """
+
     def __init__(self, repodir,arch):
         self.repodir = repodir
-        self.arch=arch
+        self.arch = arch
         self.koji_connection = self.get_koji_connection()
+        self.kojiurl = 'http://koji.fedoraproject.org/kojihub'
+        self.pkgurl = 'http://koji.fedoraproject.org/packages'
 
-    def get_koji_connection(self, url=kojiurl):
-        return koji.ClientSession(url)
+    def get_koji_connection(self):
+        """ Return a Connection to Koji hub """
+        return koji.ClientSession(self.kojiurl)
 
     def make_repo(self, packages):
+        """ Create the side repository after downloading
+        the extra packages 
+        """
         self.prep_repo_dir()
-        print 'Downloading Extra Packages'
         self.download_packages(packages)
         self.make_repo_metadata()
 
@@ -45,44 +50,42 @@ class RepoCreate(object):
         if not os.path.exists(self.repodir):
             os.makedirs(self.repodir)
 
-
     def make_repo_metadata(self):
         subprocess.call(['createrepo', self.repodir])
 
-
     def download_packages(self, packages):
+        """ Download the packages """
         rpms = []
 
         for package in packages:
             rpms.extend(self.get_rpm_urls(package))
-
         
         for rpm in rpms:
             filename = '/'.join([self.repodir, rpm['url'].split('/')[-1]])
             urllib.urlretrieve(rpm['url'], filename)
 
-
     def get_build(self, nvr):
         return self.koji_connection.getBuild(nvr)
-
 
     def get_rpms(self, build_id):
         rpms = self.koji_connection.listRPMs(build_id)
         return filter(lambda r: not r['name'].endswith('-debuginfo') and r['arch'] in self.arch, rpms)
-    
 
     def get_rpm_urls(self, nvr):
+        """ Get the download URLs of the RPMs from 
+        NVR string
+        """
 
         # get build info
         build = self.get_build(nvr)
-        pkg_name=build['package_name']
+        pkg_name = build['package_name']
 
         # list rpms per build
         rpms = self.get_rpms(build['id'])
         
         # generate list of urls
         for rpm in rpms:
-            baseurl = '/'.join((pkgurl, pkg_name,  build['version'],
+            baseurl = '/'.join((self.pkgurl, pkg_name,  build['version'],
                             build['release']))
             rpm['url'] = "%s/%s" % (baseurl, koji.pathinfo.rpm(rpm))
             
