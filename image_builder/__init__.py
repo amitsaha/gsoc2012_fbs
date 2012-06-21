16bb61bd90f927bbdde5a7f2f5a12691f18726c7
Amit Saha <amitsaha.in@gmail.com>
2012-06-21 20:15:16 +1000

    image_builder/__init__.py: cleanups

--- image_builder/__init__.py
+++ image_builder/__init__.py
@@ -13,55 +13,50 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
+# 02110-1301, USA.
 
 # Contact: Amit Saha <amitksaha@fedoraproject.org>
 #          http://fedoraproject.org/wiki/User:Amitksaha
 
-import sys
-import os
-import subprocess
 import ConfigParser
 
 from worker import Worker
 from transfer import Transfer
 
-build_config='/etc/imagebuild/imagebuild.conf'
-possible_types = ['boot','dvd','live']
+""" Kickstarts the Image Building process.
+Invoked upon Module import
+"""
 
-# Read configuration
+# Read Image Build configuration
+build_config = '/etc/imagebuild/imagebuild.conf'
 config = ConfigParser.SafeConfigParser()
 config.read(build_config)
 
-iso_type=config.get('DEFAULT','type')
-staging=config.get('DEFAULT','staging')
-email=config.get('DEFAULT','email')
+iso_type = config.get('DEFAULT', 'type')
+staging = config.get('DEFAULT', 'staging')
+email = config.get('DEFAULT', 'email')
 
-if not iso_type in possible_types:
-    print 'ISOs of type {0:s} are not supported'.format(iso_type)
-    sys.exit(1)
-
-    # boot
+# boot
 if iso_type == 'boot':
-    print 'Building Boot ISO'
-    worker=Worker(build_config)
-    imgloc=worker.build_bootiso()
-    t=Transfer(staging,imgloc)
+    worker = Worker(build_config)
+    imgloc = worker.build_bootiso()
+
+    t = Transfer(staging, imgloc)
     t.transfer_ftp()
         
-    # DVD
+# DVD
 if iso_type == 'dvd':
-    print'Building DVD'
-    worker=Worker(build_config)
-    imgloc=worker.build_dvd()
-    t=Transfer(staging,imgloc)
+    worker = Worker(build_config)
+    imgloc = worker.build_dvd()
+
+    t = Transfer(staging, imgloc)
     t.transfer_ftp()
         
-    #Live image
+#Live image
 if iso_type == 'live':
-    print 'Building Live Image'
-    worker=Worker(build_config)
-    imgloc=worker.build_live()
-    t=Transfer(staging,imgloc)
+    worker = Worker(build_config)
+    imgloc = worker.build_live()
+
+    t = Transfer(staging, imgloc)
     t.transfer_ftp()
-    
