df23a736b26f4ca228069f6160971fa553fc962a
Amit Saha <amitsaha.in@gmail.com>
2012-08-13 20:32:13 +1000

    genconf.py: config file generator template file

---
+++ genconf.py
@@ -0,0 +1,23 @@
+# Configuration file generator
+# TODO: Make this actually do something
+
+
+import ConfigParser
+
+class GenConfig:
+
+    def __init__(self):
+        pass
+
+    def genconfig(self):
+        pass
+
+
+
+if __name__=='__main__':
+    config = GenConfig()
+    config.genconfig()
+
+    
+        
+        
